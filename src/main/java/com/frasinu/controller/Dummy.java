09189dd72ed29d9167a923e074f909bbfcba4947
alexbajzat <alex.bajzat@gmail.com>
2017-05-15 21:29:39 +0300

    #1 == removed constructor package #2 == injected service in LoginController

--- src/main/java/com/frasinu/controller/Dummy.java
+++
@@ -1,7 +0,0 @@
-package com.frasinu.controller;
-
-/**
- * Created by bjz on 5/5/2017.
- */
-public class Dummy {
-}
--- src/main/java/com/frasinu/view/controllers/LoginController.java
+++ src/main/java/com/frasinu/view/controllers/LoginController.java
@@ -1,13 +1,26 @@
 package com.frasinu.view.controllers;
 
+import com.frasinu.service.UserService;
 import javafx.event.ActionEvent;
 import com.frasinu.view.FrasinuApplication;
 import com.frasinu.view.Screen;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by Paul on 5/9/17.
  */
+
+@Controller
 public class LoginController extends BaseController {
+    private UserService userService;
+
+    // autowired with setter because FXML`s controllers must have default no-args constructor
+    @Autowired
+    public void  setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
     public void goToRegister(ActionEvent actionEvent) {
         FrasinuApplication.changeScreen(Screen.REGISTER);
     }
