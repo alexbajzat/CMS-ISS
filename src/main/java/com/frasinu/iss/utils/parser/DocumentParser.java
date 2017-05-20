4a49d5f1128132969fa14c1c64168f1d40e98a7b
DESKTOP-7JD8MNN\Florin <florin.baciu96@gmail.com>
2017-05-20 17:04:43 +0300

    added docx parser

---
+++ src/main/java/com/frasinu/iss/utils/parser/DocumentParser.java
@@ -0,0 +1,38 @@
+package com.frasinu.iss.utils.parser;
+
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by Florin on 5/20/2017.
+ */
+public class DocumentParser {
+    public static PaperHolder parseDocx(InputStream inputStream) {
+
+        XWPFDocument docx = null;
+        try {
+            docx = new XWPFDocument(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        XWPFWordExtractor we = new XWPFWordExtractor(docx);
+        List<XWPFParagraph> text = docx.getParagraphs();
+
+
+        Iterator<XWPFParagraph> it = text.iterator();
+        String title = it.next().getParagraphText();
+        StringBuilder content = new StringBuilder();
+        String lineSeparator = System.getProperty("line.separator");
+        while (it.hasNext()) {
+            //line separator is appended only for new paragraphs because string builder wipes new lines
+            content.append(it.next().getParagraphText() + lineSeparator);
+        }
+        return new PaperHolder(title, content.toString());
+    }
+}
