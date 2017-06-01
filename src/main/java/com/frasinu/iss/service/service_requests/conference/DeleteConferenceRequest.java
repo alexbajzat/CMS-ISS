c9c3fa212017b6361e2d5fc65f076ec7b6d898a5
DESKTOP-JLF5BDF\Betty <melania_betty_95@yahoo.com>
2017-06-01 14:02:53 +0300

    conference management

---
+++ src/main/java/com/frasinu/iss/service/service_requests/conference/DeleteConferenceRequest.java
@@ -0,0 +1,16 @@
+package com.frasinu.iss.service.service_requests.conference;
+
+/**
+ * Created by Betty on 6/1/2017.
+ */
+public class DeleteConferenceRequest {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public DeleteConferenceRequest(int id) {
+        this.id = id;
+    }
+}
