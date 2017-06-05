902cc3b6059f0f7f290c72b31426c49d6b3e5610
alex-bajzat <alex.bajzat@gmail.com>
2017-06-05 20:44:17 +0300

    now pcController`s proposal table fills up ; changed names for more semantic shit

--- src/main/java/com/frasinu/iss/view/controllers/PCController.java
+++ src/main/java/com/frasinu/iss/view/controllers/PCController.java
@@ -1,9 +1,6 @@
 package com.frasinu.iss.view.controllers;
 
-import com.frasinu.iss.persistance.model.Author;
-import com.frasinu.iss.persistance.model.Reviewer;
-import com.frasinu.iss.persistance.model.SteeringCommitteeMember;
-import com.frasinu.iss.persistance.model.User;
+import com.frasinu.iss.persistance.model.*;
 import com.frasinu.iss.service.*;
 import com.frasinu.iss.service.service_requests.author.CreateAuthorRequest;
 import com.frasinu.iss.service.service_requests.conferenceedition.FindByConferenceEditionIdRequest;
@@ -15,40 +12,59 @@ import com.frasinu.iss.service.service_requests.user.FindIfUserIsAuthorRequest;
 import com.frasinu.iss.service.service_requests.user.UpdateUserRequest;
 import com.frasinu.iss.view.FrasinuApplication;
 import com.frasinu.iss.view.Screen;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.util.Callback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import javax.xml.bind.ValidationException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 /**
  * Created by Ericqw on 22.05.2017.
  */
 @Controller(value = "PCController")
-public class PCController extends BaseController{
+public class PCController extends BaseController {
     @FXML
-    TextField name,email,affiliation,webpage;
+    private
+    TextField name, email, affiliation, webpage;
     private ConferenceEditionService conferenceEditionService;
     private UserService userService;
     private AuthorService authorService;
     private ReviewerService reviewerService;
+    private ProposalService proposalService;
+
+
     private SteeringCommitteeMemberService steeringCommitteeMemberService;
+    @FXML
+    private TableView papersTableView;
+    @FXML
+    private TableColumn<Proposal, String> nameColumn;
+    @FXML
+    private TableColumn<Proposal, String> authorColumn;
 
     @Autowired
     public void setSteeringCommitteeMemberService(SteeringCommitteeMemberService steeringCommitteeMemberService) {
 
-        this.steeringCommitteeMemberService=steeringCommitteeMemberService;
+        this.steeringCommitteeMemberService = steeringCommitteeMemberService;
+    }
+
+    @Autowired
+    public void setProposalService(ProposalService proposalService) {
+        this.proposalService = proposalService;
     }
 
     @Autowired
     public void setReviewerService(ReviewerService reviewerService) {
-        this.reviewerService=reviewerService;
+        this.reviewerService = reviewerService;
     }
 
     @Autowired
@@ -58,67 +74,89 @@ public class PCController extends BaseController{
 
     @Autowired
     public void setUserService(UserService userService) {
-        this.userService=userService;
+        this.userService = userService;
     }
 
     @Autowired
     public void setAuthorService(AuthorService authorService) {
-        this.authorService=authorService;
+        this.authorService = authorService;
     }
 
     @Override
-    public void setData(HashMap<String, Object> data){
+    public void setData(HashMap<String, Object> data) {
         super.setData(data);
         init();
     }
 
-    public void init(){
-        int idReviewer=(int)getData().get("idReviewer");
-        int idUser=(int)getData().get("idUser");
-        User user= userService.findById(new FindByIdRequest(idUser));
-        Reviewer reviewer=reviewerService.findById(new FindReviewerByIdRequest(idReviewer));
-        if(user.getName()!=null )
+    private void init() {
+        int idReviewer = (int) getData().get("idReviewer");
+        int idUser = (int) getData().get("idUser");
+        User user = userService.findById(new FindByIdRequest(idUser));
+        Reviewer reviewer = reviewerService.findById(new FindReviewerByIdRequest(idReviewer));
+        if (user.getName() != null)
             name.setText(user.getName());
-        if(reviewer.getEmail()!=null)
+        if (reviewer.getEmail() != null)
             email.setText(reviewer.getEmail());
-        if(reviewer.getAffiliation()!=null)
+        if (reviewer.getAffiliation() != null)
             affiliation.setText(reviewer.getAffiliation());
-        if(reviewer.getWebpage()!=null)
+        if (reviewer.getWebpage() != null)
             webpage.setText(reviewer.getWebpage());
 
+        nameColumn.setCellValueFactory((proposal) -> {
+            SimpleStringProperty property = new SimpleStringProperty();
+            property.setValue(proposal.getValue().getTitle());
+            return property;
+        });
+
+        authorColumn.setCellValueFactory((proposal) -> {
+            SimpleStringProperty property = new SimpleStringProperty();
+            property.setValue(proposal.getValue().getAuthors().get(0).getUser().getName());
+            return property;
+        });
+
+        fillProposalsTable(proposalService.getAll());
     }
 
-    public void update(){
-        int idReviewer=(int)getData().get("idReviewer");
-        int idUser=(int)getData().get("idUser");
-        User user= userService.findById(new FindByIdRequest(idUser));
-        Reviewer reviewer=reviewerService.findById(new FindReviewerByIdRequest(idReviewer));
-        User newUser=User.builder().setId(user.getId())
+    private void fillProposalsTable(List<Proposal> proposals) {
+        ObservableList<Proposal> model = FXCollections.observableArrayList(proposals);
+        papersTableView.setItems(model);
+    }
+
+    public void update() {
+        int idReviewer = (int) getData().get("idReviewer");
+        int idUser = (int) getData().get("idUser");
+        User user = userService.findById(new FindByIdRequest(idUser));
+        Reviewer reviewer = reviewerService.findById(new FindReviewerByIdRequest(idReviewer));
+        User newUser = User.builder().setId(user.getId())
                 .setName(name.getText())
                 .setUsername(user.getUsername())
                 .setPassword(user.getPassword()).build();
-        Reviewer newReviewer=Reviewer.builder().setId(reviewer.getId())
+        Reviewer newReviewer = Reviewer.builder().setId(reviewer.getId())
                 .setEmail(email.getText())
                 .setAffiliation(affiliation.getText())
                 .setWebpage(webpage.getText())
                 .setUser(reviewer.getUser())
                 .setConferenceEdition(reviewer.getConferenceEdition()).build();
         try {
-            userService.updateUserPasswordEncoded(new UpdateUserRequest(newUser.getId(),newUser.getName(),newUser.getUsername(),newUser.getPassword()));
+            userService.updateUserPasswordEncoded(new UpdateUserRequest(newUser.getId(), newUser.getName(), newUser.getUsername(), newUser.getPassword()));
         } catch (ValidationException e) {
             showDialog(e.getMessage(), "Ooops!");
         }
-        reviewerService.updateReviewer(new UpdateReviewerRequest(newReviewer.getId(),newReviewer.getAffiliation(),newReviewer.getEmail(),newReviewer.getWebpage(),newReviewer.getUser(),newReviewer.getConferenceEdition()));
+        reviewerService.updateReviewer(new UpdateReviewerRequest(newReviewer.getId(), newReviewer.getAffiliation(), newReviewer.getEmail(), newReviewer.getWebpage(), newReviewer.getUser(), newReviewer.getConferenceEdition()));
     }
 
-    public void seeSchedule(javafx.event.ActionEvent ac){
-        FrasinuApplication.changeScreen(Screen.SCHEDULE,getData());
+    public void seeSchedule(javafx.event.ActionEvent ac) {
+        FrasinuApplication.changeScreen(Screen.SCHEDULE, getData());
     }
-    public void seeConferenceInfo(javafx.event.ActionEvent ac){FrasinuApplication.changeScreen(Screen.CONFERENCEINFO,getData());}
-    public void goToAuthor(javafx.event.ActionEvent ac){
 
-        Author author=userService.findIfUserIsAuthor(new FindIfUserIsAuthorRequest((int)getData().get("idUser"),(int)getData().get("idEdition")));
-        if(author==null) {
+    public void seeConferenceInfo(javafx.event.ActionEvent ac) {
+        FrasinuApplication.changeScreen(Screen.CONFERENCEINFO, getData());
+    }
+
+    public void goToAuthor(javafx.event.ActionEvent ac) {
+
+        Author author = userService.findIfUserIsAuthor(new FindIfUserIsAuthorRequest((int) getData().get("idUser"), (int) getData().get("idEdition")));
+        if (author == null) {
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
             alert.setTitle("Confirmation Dialog");
             alert.setHeaderText("You are not registered as an author");
@@ -153,16 +191,19 @@ public class PCController extends BaseController{
     }
 
     public void goToConferences(ActionEvent actionEvent) {
-        FrasinuApplication.changeScreen(Screen.CONFERENCES,getData());
+        FrasinuApplication.changeScreen(Screen.CONFERENCES, getData());
     }
-    public void bidPapers(ActionEvent actionEvent){
-        FrasinuApplication.changeScreen(Screen.BIDPAPERS,getData());
+
+    public void bidPapers(ActionEvent actionEvent) {
+        FrasinuApplication.changeScreen(Screen.BIDPAPERS, getData());
     }
-    public void makeReview(ActionEvent actionEvent){
-        FrasinuApplication.changeScreen(Screen.MAKEREVIEW,getData());
+
+    public void makeReview(ActionEvent actionEvent) {
+        FrasinuApplication.changeScreen(Screen.MAKEREVIEW, getData());
     }
-    public void seeReview(ActionEvent actionEvent){
-        FrasinuApplication.changeScreen(Screen.REVIEWS,getData());
+
+    public void seeReview(ActionEvent actionEvent) {
+        FrasinuApplication.changeScreen(Screen.REVIEWS, getData());
     }
 
 }
